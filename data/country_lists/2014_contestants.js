d1546e48fce816ba72be3d8a9ce949f2bde89f44
tom-lord <lord.thom@gmail.com>
2014-05-28 17:48:07 +0100

    Syntax correction
    Missed an "=". Oops!

--- data/country_lists/2014_contestants.js
+++ data/country_lists/2014_contestants.js
@@ -1,3 +1,3 @@
-var contestants_entered_2014=["AM","AT","AZ","BY","DK","FI","FR","DE","GR","HU","IS","IT","MT","ME","NO","PL","RO","RU","SM","SI","ES","SE","CH","NL","UA","GB"]
+var contestants_entered_2014=["AM","AT","AZ","BY","DK","FI","FR","DE","GR","HU","IS","IT","MT","ME","NO","PL","RO","RU","SM","SI","ES","SE","CH","NL","UA","GB"];
 
-var contestants_voted_only_2014["AL","BE","EE","MK","GE","IE","IL","LV","LT","MD","PT"]
+var contestants_voted_only_2014=["AL","BE","EE","MK","GE","IE","IL","LV","LT","MD","PT"];
