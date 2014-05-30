4964278761bbfbb41e32b63fd2bace1eeb11ebcb
tom-lord <lord.thom@gmail.com>
2014-05-31 00:20:01 +0100

    Jury and televote data (for 2014) added
    There is a slight issue here, which I have currently made a slight bodge
    for:
    A few countries did not have a televote,, while others do not have a
    jury vote.
    Going forward, I need to implement a more elegant workaround for this.

---
+++ data/results/2014/televote.js
@@ -0,0 +1,79 @@
+// Algeria and San Marino had no televote
+// Using jury panel votes instead
+
+var AL={ua:19, by:23, az:15, is:24, no:20, ro:16, am:13, me:5, pl:22, gr:9, at:6, de:7, se:4, fr:12, ru:25, it:2, si:14, fi:17, es:1, ch:26, hu:3, mt:10, dk:21, nl:18, sm:8, gb:11}
+
+var AM={ua:20, by:3, az:25, is:5, no:18, ro:17, me:2, pl:11, gr:10, at:24, de:4, se:22, fr:19, ru:6, it:16, si:8, fi:15, es:12, ch:13, hu:23, mt:1, dk:9, nl:7, sm:14, gb:21}
+
+var AT={ua:6, by:19, az:20, is:12, no:11, ro:5, am:3, me:22, pl:24, gr:21, de:16, se:1, fr:10, ru:15, it:23, si:9, fi:2, es:8, ch:13, hu:4, mt:14, dk:18, nl:7, sm:25, gb:17}
+
+var AZ={ua:6, by:2, is:18, no:20, ro:4, am:25, me:10, pl:11, gr:5, at:24, de:22, se:23, fr:14, ru:1, it:7, si:9, fi:15, es:12, ch:21, hu:3, mt:8, dk:17, nl:19, sm:13, gb:16}
+
+var BY={ua:4, az:3, is:17, no:11, ro:10, am:5, me:21, pl:8, gr:6, at:23, de:12, se:20, fr:18, ru:1, it:14, si:25, fi:19, es:16, ch:15, hu:9, mt:2, dk:13, nl:7, sm:24, gb:22}
+
+var BE={ua:5, by:17, az:16, is:11, no:18, ro:13, am:25, me:19, pl:26, gr:23, at:3, de:15, se:1, fr:21, ru:12, it:22, si:14, fi:4, es:10, ch:20, hu:2, mt:9, dk:6, nl:8, sm:24, gb:7}
+
+var DK={ua:9, by:21, az:10, is:11, no:6, ro:14, am:8, me:20, pl:18, gr:16, at:2, de:13, se:1, fr:17, ru:4, it:25, si:23, fi:7, es:19, ch:22, hu:12, mt:15, nl:3, sm:24, gb:5}
+
+var EE={ua:4, by:25, az:6, is:23, no:5, ro:11, am:7, me:13, pl:26, gr:22, at:8, de:15, se:1, fr:19, ru:24, it:18, si:21, fi:9, es:10, ch:17, hu:3, mt:12, dk:14, nl:2, sm:20, gb:16}
+
+var MK={ua:18, by:13, az:16, is:22, no:19, ro:3, am:10, me:2, pl:12, gr:24, at:14, de:17, se:21, fr:23, ru:5, it:4, si:15, fi:11, es:9, ch:26, hu:1, mt:7, dk:8, nl:6, sm:25, gb:20}
+
+var FI={ua:18, by:19, az:8, is:14, no:6, ro:16, am:12, me:25, pl:24, gr:21, at:1, de:15, se:2, fr:11, ru:22, it:13, si:20, es:9, ch:17, hu:10, mt:3, dk:5, nl:4, sm:23, gb:7}
+
+var FR={ua:24, by:20, az:9, is:3, no:8, ro:22, am:1, me:11, pl:15, gr:19, at:4, de:23, se:13, ru:25, it:14, si:18, fi:7, es:10, ch:16, hu:17, mt:6, dk:5, nl:2, sm:21, gb:12}
+
+var GE={ua:5, by:11, az:4, is:19, no:20, ro:15, am:1, me:25, pl:12, gr:7, at:2, de:6, se:9, fr:17, ru:3, it:18, si:26, fi:21, es:16, ch:10, hu:23, mt:22, dk:24, nl:13, sm:14, gb:8}
+
+var DE={ua:18, by:23, az:9, is:14, no:5, ro:16, am:6, me:24, pl:4, gr:22, at:11, se:7, fr:20, ru:19, it:15, si:21, fi:3, es:10, ch:12, hu:17, mt:13, dk:1, nl:2, sm:25, gb:8}
+
+var GR={ua:5, by:10, az:6, is:11, no:4, ro:18, am:12, me:21, pl:17, at:1, de:16, se:14, fr:20, ru:3, it:25, si:22, fi:7, es:23, ch:13, hu:8, mt:9, dk:19, nl:2, sm:24, gb:15}
+
+var HU={ua:10, by:18, az:2, is:11, no:21, ro:17, am:9, me:5, pl:15, gr:25, at:8, de:19, se:6, fr:24, ru:16, it:22, si:13, fi:14, es:4, ch:20, mt:12, dk:23, nl:1, sm:3, gb:7}
+
+var IS={ua:12, by:19, az:6, no:16, ro:14, am:18, me:15, pl:23, gr:21, at:2, de:17, se:4, fr:25, ru:13, it:22, si:10, fi:7, es:20, ch:24, hu:5, mt:8, dk:1, nl:3, sm:11, gb:9}
+
+var IE={ua:24, by:13, az:14, is:23, no:2, ro:18, am:4, me:22, pl:26, gr:7, at:1, de:16, se:10, fr:25, ru:20, it:19, si:12, fi:15, es:6, ch:8, hu:11, mt:5, dk:17, nl:3, sm:21, gb:9}
+
+var IL={ua:7, by:17, az:10, is:24, no:20, ro:6, am:11, me:9, pl:18, gr:5, at:1, de:25, se:2, fr:21, ru:14, it:16, si:12, fi:8, es:4, ch:22, hu:3, mt:23, dk:13, nl:19, sm:26, gb:15}
+
+var IT={ua:4, by:19, az:18, is:2, no:12, ro:13, am:24, me:14, pl:6, gr:11, at:3, de:10, se:17, fr:23, ru:20, si:7, fi:1, es:21, ch:15, hu:16, mt:8, dk:9, nl:5, sm:25, gb:22}
+
+var LV={ua:11, by:22, az:4, is:12, no:5, ro:17, am:2, me:16, pl:24, gr:26, at:10, de:9, se:3, fr:23, ru:21, it:19, si:18, fi:6, es:7, ch:20, hu:14, mt:8, dk:15, nl:1, sm:25, gb:13}
+
+var LT={ua:15, by:17, az:9, is:5, no:3, ro:25, am:20, me:19, pl:24, gr:26, at:1, de:16, se:4, fr:23, ru:10, it:21, si:11, fi:7, es:8, ch:12, hu:14, mt:18, dk:6, nl:2, sm:22, gb:13}
+
+var MT={ua:19, by:4, az:5, is:25, no:11, ro:3, am:1, me:6, pl:18, gr:8, at:9, de:21, se:10, fr:24, ru:7, it:2, si:12, fi:23, es:14, ch:13, hu:20, dk:16, nl:22, sm:17, gb:15}
+
+var MD={ua:3, by:8, az:7, is:26, no:20, ro:1, am:15, me:21, pl:12, gr:16, at:4, de:9, se:2, fr:25, ru:6, it:24, si:22, fi:23, es:10, ch:13, hu:5, mt:18, dk:17, nl:14, sm:11, gb:19}
+
+var ME={ua:8, by:9, az:19, is:15, no:25, ro:22, am:1, pl:7, gr:10, at:17, de:2, se:16, fr:11, ru:23, it:5, si:13, fi:21, es:20, ch:12, hu:3, mt:4, dk:14, nl:18, sm:6, gb:24}
+
+var NO={ua:16, by:23, az:13, is:11, ro:10, am:21, me:12, pl:19, gr:6, at:4, de:17, se:2, fr:18, ru:24, it:22, si:7, fi:3, es:5, ch:20, hu:14, mt:9, dk:15, nl:1, sm:25, gb:8}
+
+var PL={ua:16, by:18, az:10, is:12, no:5, ro:22, am:15, me:21, gr:23, at:19, de:7, se:13, fr:11, ru:20, it:24, si:17, fi:3, es:9, ch:4, hu:6, mt:2, dk:8, nl:1, sm:25, gb:14}
+
+var PT={ua:20, by:16, az:7, is:19, no:8, ro:22, am:5, me:10, pl:23, gr:24, at:6, de:11, se:3, fr:26, ru:15, it:18, si:12, fi:17, es:21, ch:4, hu:1, mt:13, dk:9, nl:2, sm:25, gb:14}
+
+var RO={ua:14, by:10, az:18, is:21, no:9, am:6, me:19, pl:23, gr:20, at:7, de:3, se:2, fr:25, ru:24, it:15, si:13, fi:12, es:5, ch:4, hu:8, mt:17, dk:1, nl:11, sm:22, gb:16}
+
+var RU={ua:6, by:1, az:2, is:13, no:10, ro:22, am:8, me:15, pl:24, gr:3, at:11, de:23, se:17, fr:21, it:25, si:16, fi:14, es:19, ch:20, hu:5, mt:4, dk:12, nl:7, sm:18, gb:9}
+
+var SM={ua:13, by:22, az:1, is:3, no:12, ro:25, am:5, me:18, pl:23, gr:21, at:14, de:11, se:2, fr:19, ru:24, it:17, si:20, fi:8, es:16, ch:15, hu:4, mt:7, dk:10, nl:9, gb:6}
+
+var SI={ua:14, by:17, az:5, is:2, no:4, ro:23, am:22, me:12, pl:10, gr:24, at:1, de:6, se:9, fr:13, ru:18, it:19, fi:15, es:7, ch:16, hu:21, mt:11, dk:8, nl:3, sm:25, gb:20}
+
+var ES={ua:5, by:24, az:19, is:9, no:7, ro:8, am:13, me:11, pl:15, gr:17, at:2, de:21, se:1, fr:18, ru:23, it:20, si:14, fi:16, ch:22, hu:4, mt:6, dk:12, nl:10, sm:25, gb:3}
+
+var SE={ua:13, by:22, az:20, is:8, no:5, ro:16, am:9, me:21, pl:17, gr:12, at:1, de:10, fr:7, ru:14, it:19, si:23, fi:6, es:18, ch:25, hu:2, mt:15, dk:4, nl:3, sm:24, gb:11}
+
+var CH={ua:20, by:23, az:22, is:17, no:4, ro:15, am:11, me:21, pl:18, gr:24, at:1, de:9, se:8, fr:13, ru:19, it:14, si:16, fi:2, es:10, hu:12, mt:6, dk:7, nl:3, sm:25, gb:5}
+
+var NL={ua:13, by:20, az:9, is:5, no:2, ro:16, am:7, me:4, pl:25, gr:24, at:1, de:18, se:3, fr:22, ru:17, it:23, si:19, fi:8, es:12, ch:11, hu:10, mt:6, dk:14, sm:21, gb:15}
+
+var UA={by:5, az:9, is:25, no:23, ro:13, am:6, me:21, pl:8, gr:7, at:3, de:2, se:1, fr:20, ru:10, it:24, si:14, fi:12, es:4, ch:18, hu:11, mt:19, dk:16, nl:15, sm:22, gb:17}
+
+var GB={ua:18, by:19, az:8, is:15, no:11, ro:22, am:24, me:21, pl:25, gr:14, at:3, de:16, se:5, fr:23, ru:10, it:17, si:6, fi:2, es:4, ch:9, hu:12, mt:1, dk:13, nl:7, sm:20}
+
+
+var from_country_2014_televote = {"AL":AL, "AM":AM, "AT":AT, "AZ":AZ, "BY":BY, "BE":BE, "DK":DK, "EE":EE, "MK":MK, "FI":FI, "FR":FR, "GE":GE, "DE":DE, "GR":GR, "HU":HU, "IS":IS, "IE":IE, "IL":IL, "IT":IT, "LV":LV, "LT":LT, "MT":MT, "MD":MD, "ME":ME, "NO":NO, "PL":PL, "PT":PT, "RO":RO, "RU":RU, "SM":SM, "SI":SI, "ES":ES, "SE":SE, "CH":CH, "NL":NL, "UA":UA, "GB":GB}
