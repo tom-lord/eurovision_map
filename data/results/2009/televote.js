13677345d2ca530208712e3d2a6fecfb83f462a8
tom-lord <lord.thom@gmail.com>
2014-06-11 23:34:12 +0100

    2009 jury and televotes FRAMEWORK
    Actual values not yet entered

---
+++ data/results/2009/televote.js
@@ -0,0 +1,86 @@
+var AL={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: hr: ro: de: se: mt: lt: es: fi: }
+
+var AD={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var BY={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var BE={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var BA={no: is: az: tr: gb: ee: gr: fr: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var HR={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: ro: de: se: mt: lt: es: fi: }
+
+var CY={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var DK={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var EE={no: is: az: tr: gb: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var MK={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var FI={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: }
+
+var FR={no: is: az: tr: gb: ee: gr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var DE={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: se: mt: lt: es: fi: }
+
+var GR={no: is: az: tr: gb: ee: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var IS={no: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var IE={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var IL={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var LV={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var LT={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: es: fi: }
+
+var MT={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: lt: es: fi: }
+
+var NL={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var NO={is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var PL={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var PT={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var RO={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: de: se: mt: lt: es: fi: }
+
+var RU={no: is: az: tr: gb: ee: gr: fr: ba: am: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var RS={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var SI={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var ES={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: fi: }
+
+var SE={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: mt: lt: es: fi: }
+
+var CH={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var TR={no: is: az: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var UA={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var GB={no: is: az: tr: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var BG={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var HU={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var MD={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var AM={no: is: az: tr: gb: ee: gr: fr: ba: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var CZ={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var ME={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var AZ={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+var SK={no: is: az: tr: gb: ee: gr: fr: ba: am: ru: ua: dk: md: pt: il: al: hr: ro: de: se: mt: lt: es: fi: }
+
+
+var from_country_2009_televote = {"AL":AL "AD":AD "BY":BY "BE":BE "BA":BA "HR":HR "CY":CY "DK":DK "EE":EE "MK":MK "FI":FI "FR":FR "DE":DE "GR":GR "IS":IS "IE":IE "IL":IL "LV":LV "LT":LT "MT":MT "NL":NL "NO":NO "PL":PL "PT":PT "RO":RO "RU":RU "RS":RS "SI":SI "ES":ES "SE":SE "CH":CH "TR":TR "UA":UA "GB":GB "BG":BG "HU":HU "MD":MD "AM":AM "CZ":CZ "ME":ME "AZ":AZ "SK":SK}
